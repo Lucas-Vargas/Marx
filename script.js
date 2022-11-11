7481cf67dc3575e281f2c262ead673c95583c701
Lucas da Silva Vargas <lucas.vargas@rede.ulbra.br>
2022-11-11 00:39:52 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+
+    /**/
+    document.querySelector("body").addEventListener("mousemove", eyeball);
+
+    function eyeball() {
+      const eye = document.querySelectorAll(".eyes");
+      eye.forEach(function (eye) {
+        let x = eye.getBoundingClientRect().left + eye.clientWidth / 2;
+        let y = eye.getBoundingClientRect().top + eye.clientHeight / 2;
+
+        let radian = Math.atan2(event.pageX - x, event.pageY - y);
+        let rotate = radian * (180 / Math.PI) * -1 + 270;
+        eye.style.transform = "rotate(" + rotate + "deg)";
+      });
+    }
+
+    document.querySelector("body").addEventListener("mousemove", armMove);
+
+    function armMove() {
+      const eye = document.querySelectorAll(".arm");
+      eye.forEach(function (eye) {
+        let x = eye.getBoundingClientRect().left + eye.clientWidth / 2;
+        let y = eye.getBoundingClientRect().top + eye.clientHeight / 2;
+
+        let radian = Math.atan2(event.pageX - x, event.pageY - y);
+        let rotate = radian * (180 / Math.PI) * -1 + 270;
+        eye.style.transform = "rotate(" + rotate + "deg)";
+      });
+    }
